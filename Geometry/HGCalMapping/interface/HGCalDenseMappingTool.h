37ad793ba74331bd8bf1ab4a38119492168fc8db
yulunmiao <yulunmiao@gmail.com>
2023-10-04 20:53:39 +0200

    [Dense Map] clang format

--- Geometry/HGCalMapping/interface/HGCalDenseMappingTool.h
+++ Geometry/HGCalMapping/interface/HGCalDenseMappingTool.h
@@ -10,14 +10,14 @@
 #define Geometry_HGCalMapping_HGCalDenseMappingTool
 #include "DataFormats/HGCalDigi/interface/HGCalElectronicsId.h"
 
-struct HGCalDenseMappingToolConfig{
-  uint32_t sLinkCaptureBlockMax{10};    ///< maximum number of capture blocks in one S-Link
-  uint32_t captureBlockECONDMax{12};    ///< maximum number of ECON-Ds in one capture block
-  uint32_t econdERXMax{12};             ///< maximum number of eRxs in one ECON-D
-  uint32_t erxChannelMax{37};           ///< maximum number of channels in one eRx
+struct HGCalDenseMappingToolConfig {
+  uint32_t sLinkCaptureBlockMax{10};  ///< maximum number of capture blocks in one S-Link
+  uint32_t captureBlockECONDMax{12};  ///< maximum number of ECON-Ds in one capture block
+  uint32_t econdERXMax{12};           ///< maximum number of eRxs in one ECON-D
+  uint32_t erxChannelMax{37};         ///< maximum number of channels in one eRx
 };
 
-class HGCalDenseMappingTool{
+class HGCalDenseMappingTool {
 public:
   HGCalDenseMappingTool(HGCalDenseMappingToolConfig);
   uint32_t denseMap(uint32_t sLink, uint32_t captureBlock);
@@ -26,9 +26,9 @@ public:
   uint32_t denseMap(uint32_t sLink, uint32_t captureBlock, uint32_t eCOND, uint32_t eRx, uint32_t channel);
   uint32_t denseMap(HGCalElectronicsId elecID);
   HGCalElectronicsId inverseDenseMap(uint32_t denseIdx);
+
 private:
   HGCalDenseMappingToolConfig config_;
-
 };
 
 #endif
